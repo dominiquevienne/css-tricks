2923bdeb5b15dec3bd5338969253d4aa8ee1bce8
Dominique Vienne <pierredominique.vienne@gmail.com>
2016-05-05 00:29:14 +0200

    Basics implementation for project

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Description of the current file
+ *
+ * @package packageName
+ * @author Dominique Vienne <pierredominique.vienne@gmail.com>
+ * @copyright Copyright (c) 2013 - Dominique Vienne - Switzerland
+ **/
+
